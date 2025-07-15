902ed727022a44b598e5f93552ae3619c06d0299
prathyushakatepalli99 <prathyushakatepalli99@gmail.com>
2025-07-15 18:59:39 +0530

    commit

--- HighestNumberServices/topic-manager/app/topic_manager.py
+++ HighestNumberServices/topic-manager/app/topic_manager.py
@@ -14,4 +14,9 @@ class TopicManager:
 
     def find_topic_high_scores(self,topic_scores_list):
       top_scores=[]
+
+      if len(topic_scores_list)==1:
+          ts=topic_scores_list[0]
+          top_score = self._highest_number_finder.find_highest_number(ts.get_scores())
+          top_sores.append(TopicTopScore(ts.get_topic_name(),top_score))
       return top_scores
--- HighestNumberServices/topic-manager/tests/test_topic_manager.py
+++ HighestNumberServices/topic-manager/tests/test_topic_manager.py
@@ -3,6 +3,11 @@ from app.topic_manager import TopicManager
 from app.topic_scores   import TopicScores
 from app.topic_top_score import TopicTopScore
 
+#Create a Test 'STUNT' Double using a STUB
+class StubHighestNumberFinder:
+    def find_highest_number(self,number):
+        return 89 #PRE-DEFINED fixed result
+
 
 class TestTopicManager(unittest.TestCase):
     def test_find_highest_score_in_empty_list(self):
@@ -33,6 +38,44 @@ class TestTopicManager(unittest.TestCase):
         #Assert
         self.assertEqual(result[0].get_topic_name(),expected_result[0].get_topic_name())
         self.assertEqual(result[0].get_topic_score(),expected_result[0].get_topic_score())
+
+
+
+    def test_find_highest_score_with_one_topic_using_stud(self):
+        scores = [45, 56, 67, 89]
+        topic_name = "Physics"
+        topic_scores = [TopicScores(topic_name, scores)]
+
+       # cut = TopicManager()
+        hnf_stub = StubHighestNumberFinder()
+        cut = TopicManager(hnf_stub)
+        expected_result = [TopicTopScore(topic_name, 89)]
+
+        # Act
+        result = cut.find_topic_high_scores(topic_scores)
+
+        # Assert
+        self.assertEqual(result[0].get_topic_name(), expected_result[0].get_topic_name())
+        self.assertEqual(result[0].get_topic_score(), expected_result[0].get_topic_score())
+
+
+    def test_find_highest_score_with_list_of_many_topics_returns_list_of_many_using_stubs(self):
+        #ARRANGE
+        physics_scores =[56,67,45,89]
+        art_scores =[87,66,78]
+        compsci_scores =[45,88,97,56]
+        topi_scores =[TopicScores("Physics",physics_scores), TopicScores("Art",art_scores),TopicScores("Compsci",compsci_scores)]
+        expected_results =[TopicScores("Physics",89), TopicScores("Art",89),TopicScores("Compsci",89)]
+        hnf_stub = StubHighestNumberFinder()
+        cut = TopicManager(hnf_stub)
+
+        #ACT
+        result = cut.find_topic_high_scores(topi_scores)
+
+        #ASSERT
+        for i in range(len(expected_results)):
+            self.assertEqual(result[i].get_topic_name(), expected_result[i].get_topic_name())
+            self.assertEqual(result[i].get_top_score(), expected_result[i].get_top_score())
 
 
 
